c39470ed5fb789437cf755608d85fec8ddc99671
arif-propsr <arif@prospr.work>
2023-12-08 15:26:30 +0500

    copy

--- frontend/build/static/js/244.ee7d929b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksqq92p=self.webpackChunksqq92p||[]).push([[244],{5244:(r,s,e)=>{e.r(s),e.d(s,{default:()=>n});e(2791);var c=e(1397),o=e(184);const n=()=>(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"Your React Page"}),(0,o.jsx)("button",{onClick:()=>{(async r=>{try{const s=await c.pb.send("/process-order/".concat(r),{method:"POST"});console.log(s.data)}catch(s){console.error("Error:",s)}})("kggqnu1xbrhrswr")},children:"Process Order"})]})}}]);
-//# sourceMappingURL=244.ee7d929b.chunk.js.map
