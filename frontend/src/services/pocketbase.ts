46fd0350e21b552aff562ecb84ff3408fd699989
bilal <bilalkhalid2240@gmail.com>
2023-12-18 07:35:19 +0500

    adding build

--- frontend/src/services/pocketbase.ts
+++ frontend/src/services/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 import { TypedPocketBase } from "../pocketbase-types";
-const apiURL =
-  process.env.REACT_APP_POCKET_BASE || "http://127.0.0.1:8090";
+const apiURL = "https://surgeryx.up.railway.app"
+  // process.env.REACT_APP_POCKET_BASE || "http://127.0.0.1:8090";
 const pb:TypedPocketBase = new PocketBase(apiURL);
 
 export { pb };
